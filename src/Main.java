7241cbc223d850e9da03316286d71b2d9b13f798
Александр <ignatenko.ap@yandex.ru>
2023-08-23 13:01:43 +0300

    homework 5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Домашнее задание 5.1");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte[] numbers = new byte[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        double[] fractionalNumbers = {1.57, 7.654, 9.986};
+        System.out.println(fractionalNumbers[0]);
+        System.out.println(fractionalNumbers[1]);
+        System.out.println(fractionalNumbers[2]);
+        int[] arbitrary = {3, 2, 36, 105};
+        System.out.println(arbitrary[3]);
+        System.out.println(arbitrary[0]);
+        System.out.println(arbitrary[2]);
+        System.out.println(arbitrary[1]);
     }
 }
